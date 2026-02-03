de247f9df35fdd78b193347f9163728883b11616
ErikJunY <erikjunyamazaki@gmail.com>
2026-02-02 21:51:01 -0300

    Feat: First commit

---
+++ src/main/java/br/com/lorenci/systeml/SystemlApplication.java
@@ -0,0 +1,13 @@
+package br.com.lorenci.systeml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lorenci/systeml/SystemlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lorenci.systeml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
